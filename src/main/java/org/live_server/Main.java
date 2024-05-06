57541c8a355754297cb5bdc6d23a9951d96b694c
sadiul-hakim <sadiulhakim@gmail.com>
2024-05-06 13:32:01 +0600

    V1.1.0

--- src/main/java/org/live_server/Main.java
+++ src/main/java/org/live_server/Main.java
@@ -3,6 +3,8 @@ package org.live_server;
 public class Main {
     public static void main(String[] args) {
 
-        Server.start(8085);
+        Thread serverThread = new Thread(new Server(8085));
+        serverThread.setName("#Server Thread");
+        serverThread.start();
     }
 }
--- src/main/resources/static/js/app.js
+++
@@ -1,4 +0,0 @@
-const button = document.getElementById("button");
-button.onclick=()=>{
-    alert("Hi")
-}
