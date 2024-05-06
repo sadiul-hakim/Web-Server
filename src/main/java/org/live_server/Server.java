a7d231f223aa2312d0388dab1c1dc9790dab8366
sadiul-hakim <sadiulhakim@gmail.com>
2024-05-06 17:02:51 +0600

    V1.2.1

--- src/main/java/org/live_server/Server.java
+++ src/main/java/org/live_server/Server.java
@@ -19,12 +19,14 @@ import java.util.stream.Stream;
 public class Server implements Runnable {
     private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
     private static final String rootFolderText = "static";
+    private static final String OS = System.getProperty("os.name");
     private final int port;
 
     public Server(int port) {
         this.port = port;
     }
 
+    // This method returns requested url
     private String requestedUrl(Socket socket) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
@@ -38,32 +40,59 @@ public class Server implements Runnable {
         return "";
     }
 
+    // This method returns all possible path (folder,file) map
     private Map<String, Path> generatePath(Path rootPath) {
 
         Map<String, Path> pathMap = new HashMap<>();
+
+        // Walk through the rootPath and find all available folder and files
         try (Stream<Path> pathStream = Files.walk(rootPath)) {
             pathStream.toList().forEach(path -> {
 
+                // Do not include the root folder
                 if (path.toString().endsWith(rootFolderText)) {
                     return;
                 }
-                int rootFolderIndex = path.toString().indexOf(rootFolderText);
-                String nextToStatic = path.toString()
-                        .substring(rootFolderIndex + (rootFolderText.length()) + 1);
-                String[] actualFolderPath = nextToStatic.split("\\\\");
-                StringBuilder pathBuilder = new StringBuilder();
-                for (String s : actualFolderPath) {
-                    pathBuilder.append("/")
-                            .append(s);
-                }
-                if (!pathMap.containsKey(pathBuilder.toString().replaceFirst("/", ""))) {
-                    pathMap.put(pathBuilder.toString().replaceFirst("/", ""), path);
+
+                String actualPath = getActualPath(path);
+                 if (!pathMap.containsKey(actualPath)) {
+
+                    // As we are not showing folders, Do not include folders in the map
+                    if (actualPath.contains(".")) {
+                        pathMap.put(actualPath, path);
+                    }
                 }
             });
             return pathMap;
         } catch (Exception ex) {
             return Collections.emptyMap();
         }
+    }
+
+    private String getActualPath(Path path) {
+
+        // Get the root folder index from the full path
+        int rootFolderIndex = path.toString().indexOf(rootFolderText);
+
+        // Get the path next to root path
+        String nextToRootFolder = path.toString()
+                .substring(rootFolderIndex + (rootFolderText.length()) + 1);
+        String[] actualFolderPath;
+
+        if (OS.toLowerCase().contains("windows")) {
+            actualFolderPath = nextToRootFolder.split("\\\\");
+        } else {
+            actualFolderPath = nextToRootFolder.split("/");
+        }
+
+        // Build path
+        StringBuilder pathBuilder = new StringBuilder();
+        for (String s : actualFolderPath) {
+            pathBuilder.append("/")
+                    .append(s);
+        }
+
+        return pathBuilder.toString().replaceFirst("/", "");
     }
 
     private void sendResponse(OutputStream outputStream, Path path) throws IOException {
@@ -96,7 +125,7 @@ public class Server implements Runnable {
 
             // As the server is not live and user needs to restart everytime some is changes
             // There is no need to generate the root path on each iteration.
-            Map<String, Path> pathMap = generatePath(rootPath);;
+            Map<String, Path> pathMap = generatePath(rootPath);
             String requestUrl;
             Path resultPath;
             while (true) {
