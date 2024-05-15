5a0e1a43c60e247370782745fac80a6ebde03989
sadiul-hakim <sadiulhakim@gmail.com>
2024-05-15 16:24:12 +0600

    Server without Controller

--- src/main/java/org/live_server/Main.java
+++ src/main/java/org/live_server/Main.java
@@ -1,10 +1,36 @@
 package org.live_server;
 
-public class Main {
-    public static void main(String[] args) {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
 
-        Thread serverThread = new Thread(new Server(9095));
-        serverThread.setName("#Server Thread");
-        serverThread.start();
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+public class Main {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) {
+        try {
+            var server = HttpServer.create(new InetSocketAddress(9095),0);
+            System.out.println("Server is running on port : 9095");
+
+            var context = server.createContext("/");
+            context.setHandler(Main::handleRequests);
+            server.start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void handleRequests(HttpExchange exchange) throws IOException{
+        String response = "<h1 style='background:crimson;color:white;text-align:center;padding:20px;'>Welcome To the Home Page</h1>";
+        exchange.sendResponseHeaders(200,response.getBytes().length);
+        exchange.setAttribute("Content-Type","text/html");
+        var os = exchange.getResponseBody();
+        os.write(response.getBytes(StandardCharsets.UTF_8));
+        os.close();
     }
 }
